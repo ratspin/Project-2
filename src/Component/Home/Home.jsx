efc0ed1f07f2b6fbaac97ec69a4793fbcb40df9b
ratspin <6210110013@psu.ac.th>
2023-02-28 20:28:08 +0700

    icon

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -6,11 +6,11 @@ import { HeaderCon,Logo} from './Styled'
 export default function Home() {
   return (
     <HeaderCon>
-        <Logo src="/food4U.png" />
+        <Logo src="/3.png" />
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center' }}>
             <div><br/>
                 <h1>&nbsp;</h1>
-                <h1 style={{ fontSize: '4rem', color: '#fff' }}>แนะนำอาหารสำหรับผู้ป่วยสูงอายุ<br />ที่เป็นโรคไม่ติดต่อเรื้อรัง</h1>
+                <h1 style={{ fontSize: '4rem', color: '#fff' }}>ระบบแนะนำอาหารสำหรับผู้ป่วยสูงอายุ<br />ที่เป็นโรคไม่ติดต่อเรื้อรัง</h1>
                 <h3 style={{ fontSize: '1.5rem', color: '#fff', fontWeight: 'normal', marginTop: '1rem' }}>เมนูอาหารทางเลือกเพื่อสุขภาพ</h3>
                 <p style={{ fontSize: '1.2rem', color: '#fff', fontWeight: 'normal', marginTop: '1.5rem' }}>วางแผนการรับประทานอาหารสำหรับคุณ</p>
                 <button style={{ marginTop: '1rem', backgroundColor: '#f8ae1a', color: '#fff', padding: '20px 1rem', width: '200px', border: 'none', fontSize: '1.5rem', position: 'relative', top: '5px' ,cursor: 'pointer', }} type='submit'>
--- src/Component/Home/Styled.jsx
+++ src/Component/Home/Styled.jsx
@@ -1,10 +1,8 @@
 import styled from 'styled-components'
 
-import HeaderBG from '../../assets/tom-yum-kung.jpg'
-
 export const HeaderCon = styled.section`
     height: 750px;
-    background: url(${HeaderBG});
+    background: url(/tom-yum-kung.jpg);
     box-shadow: inset 0 0 0 2000px rgba(0, 0, 0, 0.6);
     border-bottom: 3px solid #222;
     background-position: center;
